47f3133d95e04400373eadae667cec84fcd1be20
Mr. R <roberto.recopelacion@gmail.com>
2024-03-10 00:29:17 +0800

    update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -14,31 +14,31 @@
 
 get_header();
 ?>
-<main class="s-container">
-	<div class="m-content-title">
-		<h1 class="m-content-title-text">お問い合わせ<span class="en">CONTACT</span></h1>
+<main class="suwarikuSContainer">
+	<div class="suwarikuContentTitle">
+		<h1 class="suwarikuContentTitle-text">お問い合わせ<span class="en">CONTACT</span></h1>
 	</div>
-<div class="m-breadcrumb-wrap">
-	<ol class="m-breadcrumb">
-		<li class="m-breadcrumb__item"><a href="<?php bloginfo('url'); ?>">ホーム</a></li>
-		<li class="m-breadcrumb__item">お問い合わせ</li>
+<div class="suwarikuBreadcrumb-wrap">
+	<ol class="suwarikuBreadcrumb">
+		<li class="suwarikuBreadcrumb__item"><a href="<?php bloginfo('url'); ?>">ホーム</a></li>
+		<li class="suwarikuBreadcrumb__item">お問い合わせ</li>
 	</ol>
 </div>
 
 <section>
-	<div class="l-inner m-pt-xl m-pb-3xl">
-		<dl> <dt class="m-text-18 m-align-c">お電話でのお問い合わせはこちら</dt>
-		<dd class="m-mt-xs">
-			<ul class="m-contact-us-tel">
-			<li class="m-contact-us-tel__item"><span class="m-text-18">代表</span><i class="icon"><img src="<?php echo get_template_directory_uri(); ?>/resources/images/contact/icon-tel.svg" alt="" width="22" /></i><span class="m-text-30"><a href="tel:0266-52-6193">0266-52-6193</a></span></li>
+	<div class="suwarikuInner suwarikuPtop-xl suwarikuPBtm-3xl">
+		<dl> <dt class="suwarikuText-18 suwarikuAlignC">お電話でのお問い合わせはこちら</dt>
+		<dd class="suwarikuMt-xs">
+			<ul class="suwarikuContactUsTel">
+			<li class="suwarikuContactUsTel__item"><span class="suwarikuText-18">代表</span><i class="icon"><img src="<?php echo get_template_directory_uri(); ?>/resources/images/contact/icon-tel.svg" alt="" width="22" /></i><span class="suwarikuText-30"><a href="tel:0266-52-6193">0266-52-6193</a></span></li>
 			</ul>
 		</dd>
-		<dd class="m-mt-xs m-text-18 m-align-c">営業時間/8:50～17:30　休業日/土曜日・日曜日・祝日</dd>
+		<dd class="suwarikuMt-xs suwarikuText-18 suwarikuAlignC">営業時間/8:50～17:30　休業日/土曜日・日曜日・祝日</dd>
 		</dl>
-		<hr class="m-hr m-mt-l" />
-		<div class="m-mt-l">
-		<h2 class="m-align-c m-text-24">お問い合わせ・ご意見・ご感想はこちらからお寄せください。</h2>
-		<p class="m-align-c m-text-18 m-mt-xs">[ <a class="m-uline" href="<?php bloginfo('url'); ?>/privacy-policy/">プライバシーポリシー</a> ]</p>
+		<hr class="suwarikuHr suwarikuMt-l" />
+		<div class="suwarikuMt-l">
+		<h2 class="suwarikuAlignC suwarikuText-24">お問い合わせ・ご意見・ご感想はこちらからお寄せください。</h2>
+		<p class="suwarikuAlignC suwarikuText-18 suwarikuMt-xs">[ <a class="suwarikuULine" href="<?php bloginfo('url'); ?>/privacy-policy/">プライバシーポリシー</a> ]</p>
 		<?php echo do_shortcode('[mwform_formkey key="29"]'); ?>
 	</div>
 </section>
