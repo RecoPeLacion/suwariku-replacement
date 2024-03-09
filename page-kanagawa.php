251b33269140b6956c08bac6f6a6259ef367cb28
Mr. R <roberto.recopelacion@gmail.com>
2024-03-10 07:43:28 +0800

    update class page-kanagawa.php

--- page-kanagawa.php
+++ page-kanagawa.php
@@ -14,67 +14,67 @@
 
 get_header();
 ?>
-<main class="l-container">
-	<div class="m-content-title">
-		<h1 class="m-content-title-text">輸送業<span class="en">TRANSPORTION</span></h1>
+<main class="suwarikuContainer">
+	<div class="suwarikuContentTitle">
+		<h1 class="suwarikuContentTitle-text">輸送業<span class="en">TRANSPORTION</span></h1>
 	</div>
-	<div class="m-breadcrumb-wrap">
-		<ol class="m-breadcrumb">
-			<li class="m-breadcrumb__item"><a href="<?php bloginfo('url'); ?>">ホーム</a></li>
-			<li class="m-breadcrumb__item">事業紹介</li>
-			<li class="m-breadcrumb__item">輸送業</li>
-			<li class="m-breadcrumb__item">神奈川営業所</li>
+	<div class="suwarikuBreadcrumb-wrap">
+		<ol class="suwarikuBreadcrumb">
+			<li class="suwarikuBreadcrumb__item"><a href="<?php bloginfo('url'); ?>">ホーム</a></li>
+			<li class="suwarikuBreadcrumb__item">事業紹介</li>
+			<li class="suwarikuBreadcrumb__item">輸送業</li>
+			<li class="suwarikuBreadcrumb__item">神奈川営業所</li>
 		</ol>
 	</div>
 
 	<section>
-		<div class="l-inner m-pt-l m-pb-l tr-main-img">
-			<h2 class="m-mt-xl m-line-title trans-pa"><span class="title">神奈川営業所</span></h2>
+		<div class="suwarikuInner suwarikuPtop-l suwarikuPBtm-l suwarikuTrMainImg">
+			<h2 class="suwarikuMt-xl suwarikuLineTitle suwarikuTransPa"><span class="title">神奈川営業所</span></h2>
 			<img src="<?php echo get_template_directory_uri(); ?>/resources/images/transportion/kanagawa.jpg">
 		</div>
 	</section>
 
 	<section>
-		<div class="l-inner m-pt-l m-pb-l">
-			<h2 class="m-mt-xl m-line-title trans-pa"><span class="title">ご挨拶</span></h2>
-			<div class="greeting-box trans-pa">
-				<div class="greeting-box_img">
+		<div class="suwarikuInner suwarikuPtop-l suwarikuPBtm-l">
+			<h2 class="suwarikuMt-xl suwarikuLineTitle suwarikuTransPa"><span class="title">ご挨拶</span></h2>
+			<div class="suwarikuGreetingBox suwarikuTransPa">
+				<div class="suwarikuGreetingBox_img">
 					<img src="<?php echo get_template_directory_uri(); ?>/resources/images/transportion/chief_05.jpg">
 				</div>
-				<div class="greeting-box_txt">
+				<div class="suwarikuGreetingBox_txt">
 					<p>神奈川営業所は冷凍食品に特化した物流業務で、24時間稼働の冷凍・冷蔵物流センターです。<br><br>
 					高品質で、お客様に荷物をお届けする事を大切にしています。<br><br>
 					輸送は冷凍・冷蔵食品の輸送がメインですが、医療関係・精密資材の輸送を高品質で、安心・安全をお届けしています。<br><br>
 					輸送・保管のご依頼等は、お気軽にご相談ください。
 					</p>
-					<p class="greeting-box_txt__name">所長 センター長　林 政仁</p>
+					<p class="suwarikuGreetingBox_txt__name">所長 センター長　林 政仁</p>
 				</div>
 			</div>
-			<p class="m-text-24 m-text-lh-l m-font-serif m-align-c m-align-c--pc">
-				大手スーパー様、ドラックストアー様、<br class="m-pc-only">外食店様の納品センターとして365日、24時間活動しています！！
+			<p class="suwarikuText-24 suwarikuText-lh-l suwarikuFontSerif suwarikuAlignC suwarikuAlignC--pc">
+				大手スーパー様、ドラックストアー様、<br class="suwarikuPconly">外食店様の納品センターとして365日、24時間活動しています！！
 			</p>
 		</div>
 	</section>
 	<section>
-		<div class="l-inner m-pt-l m-pb-l">
-			<h3 class="m-service-warehouse-item-title m-font-b m-text-30">物流（座間）</h3>
-			<div class="kanagawa-box">
-				<div class="kanagawa-box_img">
+		<div class="suwarikuInner suwarikuPtop-l suwarikuPBtm-l">
+			<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30">物流（座間）</h3>
+			<div class="suwarikuKanagawaBox">
+				<div class="suwarikuKanagawaBox_img">
 					<img src="<?php echo get_template_directory_uri(); ?>/resources/images/transportion/kana_img-01.png">
 				</div>
-				<div class="kanagawa-box_txt">
+				<div class="suwarikuKanagawaBox_txt">
 					<p>
 						国産鶏肉を中心に各種畜産品に特化した、保管・配送湿度（パーシャル帯）の設定で入出庫・保管・在庫管理・荷受管理・荷受等を行なっております。<br><br>
 						関東圏全域で共同配送も行なっており、今期は、12t冷凍車・4t冷凍車を増車して更なる業務拡大をしております。
 					</p>
 				</div>
 			</div>
-			<h3 class="m-service-warehouse-item-title m-font-b m-text-30">運行（相模原）</h3>
-			<div class="kanagawa-box">
-				<div class="kanagawa-box_img">
+			<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30">運行（相模原）</h3>
+			<div class="suwarikuKanagawaBox">
+				<div class="suwarikuKanagawaBox_img">
 					<img src="<?php echo get_template_directory_uri(); ?>/resources/images/transportion/kana_img-02.jpg">
 				</div>
-				<div class="kanagawa-box_txt">
+				<div class="suwarikuKanagawaBox_txt">
 					<p>
 						食品配送に中心に、医薬品・アルミ鋼鉄材など様々ななジャンルでの配送を行い、お客様からお預かりした商品を大切にお届け致します。<br>
 						車庫は、営業所より少し離れた”相模原”に位置していますが、安心して業務を行える体制・環境を整えています。
@@ -84,8 +84,8 @@ get_header();
 		</div>
 	</section>
 	<section>
-		<div class="l-inner m-pt-l m-pb-l">
-			<table class="m-tbl m-mt-xl m-text-16">
+		<div class="suwarikuInner suwarikuPtop-l suwarikuPBtm-l">
+			<table class="suwarikuTbl suwarikuMt-xl suwarikuText-16">
 				<tr>
 					<th>住所</th>
 					<td>〒252-0001 神奈川県座間市小松原1-1-43</td>
@@ -123,7 +123,7 @@ get_header();
 					<td>喫煙所、休憩所、更衣室、シャワールーム</td>
 				</tr>
 			</table>
-			<h2 class="m-mt-xl m-line-title trans-pa"><span class="title">アクセス</span></h2>
+			<h2 class="suwarikuMt-xl suwarikuLineTitle suwarikuTransPa"><span class="title">アクセス</span></h2>
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3248.2597554132444!2d139.42289747575987!3d35.49785477264706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018ff15e323a8bf%3A0xcc019f205d179764!2z44CSMjUyLTAwMDIg56We5aWI5bed55yM5bqn6ZaT5biC5bCP5p2-5Y6f77yR5LiB55uu77yR4oiS77yU77yTIOODiOODvOOCq-OCpOODu-ODkeODg-OCseODvOOCuOW6p-mWk-eJqea1geOCu-ODs-OCv-ODvA!5e0!3m2!1sja!2sjp!4v1684490185134!5m2!1sja!2sjp" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 	</section>
 
