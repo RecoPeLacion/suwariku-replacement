9ba68b6ce7115c0852a92e26cda5e4e55c76f970
Mr. R <roberto.recopelacion@gmail.com>
2024-03-10 10:11:13 +0800

    update class page-torikumi.php

--- page-torikumi.php
+++ page-torikumi.php
@@ -14,62 +14,62 @@
 
 get_header();
 ?>
-<main class="l-container">
-	<div class="m-content-title">
-		<h1 class="m-content-title-text">スワリクの取り組み<span class="en">ATTEMPT</span></h1>
+<main class="suwarikuContainer">
+	<div class="suwarikuContentTitle">
+		<h1 class="suwarikuContentTitle-text">スワリクの取り組み<span class="en">ATTEMPT</span></h1>
 	</div>
-	<div class="m-breadcrumb-wrap">
-		<ol class="m-breadcrumb">
-			<li class="m-breadcrumb__item"><a href="<?php bloginfo('url'); ?>">ホーム</a></li>
-			<li class="m-breadcrumb__item">取り組み</li>
+	<div class="suwarikuBreadcrumb-wrap">
+		<ol class="suwarikuBreadcrumb">
+			<li class="suwarikuBreadcrumb__item"><a href="<?php bloginfo('url'); ?>">ホーム</a></li>
+			<li class="suwarikuBreadcrumb__item">取り組み</li>
 		</ol>
 	</div>
 	<section id="trafic-safty">
-		<div class="a-content-title">
-			<h1 class="m-content-title-text a-main-title t-color-01" id="warehouse">交通安全<span
-					class="en a-sub-title t-color-01_sec">TRAFIC
+		<div class="suwarikuAContentTitle">
+			<h1 class="suwarikuContentTitle-text suwarikuAMainTitle suwarikuTColor-01" id="warehouse">交通安全<span
+					class="en suwarikuSubTitle suwarikuTColor-01_sec">TRAFIC
 					SAFTY</span></h1>
 		</div>
 		<div id="attemp-com-box">
-			<div class="l-inner m-pb-3xl">
-				<div class="trafic-box-item">
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt sp-pa">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30">Gマーク表彰（交通安全性流量事業所）</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+			<div class="suwarikuInner suwarikuPBtm-3xl">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt suwarikuSpPa">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30">Gマーク表彰（交通安全性流量事業所）</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>Gマークとは、守るべき交通法制をしっかりと守り、安全性向上の取り組みをしていると認定されたトラック運送事業者が使用できるシンボルマークのことです。スワリクでは毎年Gマーク表彰をいただいとております。これからも安全に事業を行うことを大切にお元気をお届けします！！！
 							</p>
 						</div>
 					</div>
-					<div class="trafic-box-item_img">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png"
 							alt="">
 					</div>
 				</div>
-				<div class="trafic-box-item">
-					<div class="trafic-box-item_img">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/lunchmeet.png"
 							alt="">
 					</div>
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt sp-pa">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30">安全会議・ランチミーティング</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt suwarikuSpPa">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30">安全会議・ランチミーティング</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>スワリクでは、全員参加の経営を志しにしています。
 								その為、従業員一人一人がスワリクの顔であることを自覚して業務を行っております。安全会議・ランチミーティングでは、日ごろの業務について反省点、改善点を話し合い、更によりより会社を目指しております。
 							</p>
 						</div>
 					</div>
 				</div>
-				<div class="trafic-box-item">
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30">子供ミュージアム</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30">子供ミュージアム</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>「子供ミュージアム」とは、一般社法人団様の主催で行われているプロジェクトの一環で、当社の所有しておりますトラックに地元や地域のお子様の絵を飾り走ることで、運転手に心のゆとりを抱いて、少しでも悲しい事故がなくなるようにする取り組みです。
 							</p>
 						</div>
 					</div>
-					<div class="trafic-box-item_img">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/museum.png"
 							alt="">
@@ -82,21 +82,21 @@ get_header();
 
 
 	<section id="environmentar-activities">
-		<div class="a-content-title">
-			<h1 class="m-content-title-text a-main-title t-color-02" id="warehouse">環境活動<span
-					class="en a-sub-title t-color-02_sec">ENVIRONMENTAL ACTIVITIES</span></h1>
+		<div class="suwarikuAContentTitle">
+			<h1 class="suwarikuContentTitle-text suwarikuAMainTitle suwarikuTColor-02" id="warehouse">環境活動<span
+					class="en suwarikuSubTitle suwarikuTColor-02_sec">ENVIRONMENTAL ACTIVITIES</span></h1>
 		</div>
 		<div id="attemp-com-box">
-			<div class="l-inner m-pb-3xl">
-				<div class="trafic-box-item">
-					<div class="trafic-box-item_img">
+			<div class="suwarikuInner suwarikuPBtm-3xl">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/adapt.png"
 							alt="">
 					</div>
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt sp-pa">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30 envir-title-line">諏訪湖アダプト</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt suwarikuSpPa">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30 suwarikuEnvirTitleLine">諏訪湖アダプト</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>平成14年から諏訪湖アダプトという活動に参加しております。
 								毎年多くのドライバーさんに協力していただき、諏訪湖周辺のゴミ拾いを行っています。諏訪湖がさらにきれいになるよう毎年の活動を欠かさず続けております。
 								各事業所で、花壇の整備を行い、訪れるお客様に気持ちの良い綺麗な環境でお迎えすることを心がけております。
@@ -106,16 +106,16 @@ get_header();
 						</div>
 					</div>
 				</div>
-				<div class="trafic-box-item">
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30  envir-title-line">環境整備</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30  suwarikuEnvirTitleLine">環境整備</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>各事業所で、花壇の整備を行い、訪れるお客様に気持ちの良いきれいな環境でお迎えすることを心掛けております。
 								当社にお越しの際は、ぜひご覧になってください。
 							</p>
 						</div>
 					</div>
-					<div class="trafic-box-item_img">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/environmental.png"
 							alt="">
@@ -127,61 +127,61 @@ get_header();
 	</section>
 
 	<section id="regionalactivity">
-		<div class="a-content-title">
-			<h1 class="m-content-title-text a-main-title t-color-03" id="warehouse">地域活動<span
-					class="en a-sub-title t-color-02_sec">REGIONAL ACTIVITIES</span></h1>
+		<div class="suwarikuAContentTitle">
+			<h1 class="suwarikuContentTitle-text suwarikuAMainTitle suwarikuTColor-03" id="warehouse">地域活動<span
+					class="en suwarikuSubTitle suwarikuTColor-02_sec">REGIONAL ACTIVITIES</span></h1>
 		</div>
 		<div id="attemp-com-box">
-			<div class="l-inner m-pb-3xl">
-				<div class="trafic-box-item">
-					<div class="trafic-box-item_img">
+			<div class="suwarikuInner suwarikuPBtm-3xl">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/regional01.png"
 							alt="">
 					</div>
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt sp-pa">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30 regional-title-line">御柱の看板</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt suwarikuSpPa">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30 regional-title-line">御柱の看板</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>平成28年に御柱祭が開催され、その時に作成した看板です。
 								「ようこそ、神の里へ」という文字とともに御柱に関わる皆様のお写真を頂戴し看板にさせていただきました。
 							</p>
 						</div>
 					</div>
 				</div>
-				<div class="trafic-box-item">
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt sp-pa">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30 regional-title-line">新作花火</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt suwarikuSpPa">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30 regional-title-line">新作花火</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>諏訪湖花火大会は、日本一の打ち上げ数を誇っています。
 							</p>
 						</div>
 					</div>
-					<div class="trafic-box-item_img">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/regional02.png"
 							alt="">
 					</div>
 				</div>
-				<div class="trafic-box-item">
-					<div class="trafic-box-item_img">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/regional03.png"
 							alt="">
 					</div>
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt sp-pa">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30 regional-title-line">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt suwarikuSpPa">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30 regional-title-line">
 							地元小学校へのニュース掲示板（2014年）</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>
 								地元の豊田小学校に朝日写真ニュース1年分と専用掲示板を寄贈いたしました。全ての人が交通法制に配慮して事故を無くす努力をしていきましょう。豊田小学校には、コドモミュージアムでもお世話になっておりますので、これからもともに交通安全に努めていく所存です。
 							</p>
 						</div>
 					</div>
 				</div>
-				<div class="trafic-box-item">
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30 regional-title-line">職場体験</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30 regional-title-line">職場体験</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>
 								長野市立川中島中学校の職場体験の受け入れをしました。
 								生徒さんたちは初め緊張しておりましたが、作業内容を直ぐに理解し率先して行動してくださいました。
@@ -189,7 +189,7 @@ get_header();
 							</p>
 						</div>
 					</div>
-					<div class="trafic-box-item_img">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/regional04.png"
 							alt="">
@@ -201,20 +201,20 @@ get_header();
 	</section>
 
 	<section id="6sactivity">
-		<div class="a-content-title">
-			<h1 class="m-content-title-text a-main-title t-color-04" id="warehouse">6S活動<span
-					class="en a-sub-title t-color-02_sec">6S ACTIVITY</span></h1>
+		<div class="suwarikuAContentTitle">
+			<h1 class="suwarikuContentTitle-text suwarikuAMainTitle suwarikuTColor-04" id="warehouse">6S活動<span
+					class="en suwarikuSubTitle suwarikuTColor-02_sec">6S ACTIVITY</span></h1>
 		</div>
 		<div id="attemp-com-box">
-			<div class="l-inner m-pb-3xl">
-				<div class="trafic-box-item">
-					<div class="trafic-box-item_img">
+			<div class="suwarikuInner suwarikuPBtm-3xl">
+				<div class="suwarikuTrafficBoxItem">
+					<div class="suwarikuTrafficBoxItem_img">
 						<!-- <img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/commendation.png" width="472" height="300" alt=""> -->
 						<img src="<?php echo get_template_directory_uri(); ?>/resources/images/attempt/6s.png" alt="">
 					</div>
-					<div class="m-service-warehouse-item-col__content trafic-box-item_txt">
-						<h3 class="m-service-warehouse-item-title m-font-b m-text-30 s6-title-line">6S活動への取り組み</h3>
-						<div class="m-service-warehouse-item-content m-mt">
+					<div class="suwarikuServiceWarehouseItem-col__content suwarikuTrafficBoxItem_txt">
+						<h3 class="suwarikuServiceWarehouseItem-title suwarikuFontB suwarikuText-30 s6-title-line">6S活動への取り組み</h3>
+						<div class="suwarikuServiceWarehouseItem-content suwarikuMt">
 							<p>1.整理・・・必要のないものは処分する<br>
 								2.整頓・・・直ぐに活用できる置き方をする<br>
 								3.掃除・・・常に掃除してきれいな職場環境を保つ<br>
