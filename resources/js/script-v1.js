b0a2ddbcc75842ad453fc53c5374f11a0dee2aab
Mr. R <roberto.recopelacion@gmail.com>
2024-03-23 18:59:55 +0800

    reorder the script and update the comment in script.js

--- resources/js/script-v1.js
+++ resources/js/script-v1.js
@@ -1,18 +1,10 @@
 
-/*--------------------------------------------------------------------------
-onload add
---------------------------------------------------------------------------*/
-function addOnload(func) {
-	try {
-		window.addEventListener("load", func, false);
-	} catch (e) {
-		window.attachEvent("onload", func);
-	}
+// Add Load Browser
+function addLoadBrowser(func) {
+    $(window).on("load", func);
 }
 
-/* --------------------------------------------------------
-user agent
--------------------------------------------------------- */
+// User Agent
 
 var ua = {};
 ua.name = window.navigator.userAgent.toLowerCase();
@@ -21,53 +13,7 @@ ua.ver = window.navigator.appVersion.toLowerCase();
 
 ;(function($){
 
-/*--------------------------------------------------------------------------
-common effect
---------------------------------------------------------------------------*/
-
-	//scroll
-	$.fn.smoothScroll = function(options){
-		var c = $.extend({
-			speed : 400,
-			easing : 'swing'
-		}, options);
-		this.on('click',function(e){
-			e.preventDefault();
-			var $elmHash = this.hash;
-			if(!$elmHash || $elmHash === "#") {
-				return false;
-			};
-			var targetOffset = $($elmHash).offset().top - $('.suwarikuHeader').innerHeight();
-			$('html,body').animate({scrollTop: targetOffset}, c.speed, c.easing);
-		});
-	}
-	$("a[href^='#']").smoothScroll();
-
-	function ankerSet(){
-		var urlHash = location.hash.replace(/<[^>]+>/g, '');
-		if(urlHash.indexOf('#') != -1 && $(urlHash)[0]){
-			$('html,body').animate({scrollTop: $(urlHash).offset().top - $('.suwarikuHeader').innerHeight()}, 1);
-		}
-	}
-	addOnload(ankerSet);
-
-	//popup sns
-	$('.js-puwin').on("click",function(e){
-		e.preventDefault();
-		window.open(this.href,"WindowName","width=520,height=520,resizable=yes,scrollbars=yes");
-		return false;
-	});
-
-	//global nav
-	$('.suwarikuHeaderMenu').on("click",function(e){
-		e.preventDefault();
-		$(this).toggleClass('is-active');
-		$('.suwarikuHeader').toggleClass('is-active');
-		$('.suwarikuHeadernav').toggleClass('is-active');
-		return false;
-	});
-
-	//scroll anima
+	//Add Scroll Animation
 	function scrollHeader(){
 		var scrollH = $(window).scrollTop();
 		var pos = 240;
@@ -97,146 +43,7 @@ common effect
 		}
 	});
 
-	//slider
-
-	function warehouseDetailSlider() {
-		if($('.suwarikuServiceWarehouseDetailSlider')[0]){
-			$('.suwarikuServiceWarehouseDetailSlider').slick({
-				fade: true,
-				slidesToShow: true,
-				slidesToScroll:1,
-				// centerMode: true,
-				adaptiveHeight: true,
-				initialSlide: 0, //start number
-				cssEase: 'ease-in-out',
-				infinite: true,
-				arrows: false,
-				dots: true,
-				autoplay: true,
-				autoplaySpeed: 2000,
-				speed: 500,
-				slide: '.suwarikuServiceWarehouseDetailSliderItem',
-				// prevArrow: '<i class="m-icon-slide-prev">前へ</i>',
-				// nextArrow: '<i class="m-icon-slide-next">次へ</i>',
-				pauseOnHover: false,
-				draggable: false,
-				touchThreshold: 10,
-				swipe: false,
-				// swipeToSlide: false,
-				touchMove: false
-			});
-			// $('.suwarikuHomeMv-list').slick("slickPause");
-			// $('.suwarikuHomeMv-list').addClass('is-load');
-			// $('.suwarikuHomeMv-list').slick("slickPlay");
-		}
-	}
-	warehouseDetailSlider();
-
-
-	function realestateDetailSlider() {
-		if($('.suwarikuServiceRealstateImg')[0]){
-			$('.suwarikuServiceRealstateImg').slick({
-				fade: true,
-				slidesToShow: true,
-				slidesToScroll:1,
-				// centerMode: true,
-				adaptiveHeight: true,
-				initialSlide: 0, //start number
-				cssEase: 'ease-in-out',
-				infinite: true,
-				arrows: false,
-				dots: true,
-				autoplay: true,
-				autoplaySpeed: 2000,
-				speed: 500,
-				slide: '.suwarikuServiceRealstateImgItem',
-				// prevArrow: '<i class="m-icon-slide-prev">前へ</i>',
-				// nextArrow: '<i class="m-icon-slide-next">次へ</i>',
-				pauseOnHover: false,
-				draggable: false,
-				touchThreshold: 10,
-				swipe: false,
-				// swipeToSlide: false,
-				touchMove: false
-			});
-			// $('.suwarikuHomeMv-list').slick("slickPause");
-			// $('.suwarikuHomeMv-list').addClass('is-load');
-			// $('.suwarikuHomeMv-list').slick("slickPlay");
-		}
-	}
-	realestateDetailSlider();
-
-
-	function csrSlider() {
-		if($('.suwarikuCsrSlider')[0]){
-			$('.suwarikuCsrSlider').slick({
-				fade: true,
-				slidesToShow: true,
-				slidesToScroll:1,
-				// centerMode: true,
-				adaptiveHeight: true,
-				initialSlide: 0, //start number
-				cssEase: 'ease-in-out',
-				infinite: true,
-				arrows: false,
-				dots: false,
-				autoplay: true,
-				autoplaySpeed: 2000,
-				speed: 1500,
-				slide: '.suwarikuCsrSliderItem',
-				// prevArrow: '<i class="m-icon-slide-prev">前へ</i>',
-				// nextArrow: '<i class="m-icon-slide-next">次へ</i>',
-				pauseOnHover: false,
-				draggable: false,
-				touchThreshold: 10,
-				swipe: false,
-				// swipeToSlide: false,
-				touchMove: false
-			});
-			// $('.suwarikuHomeMv-list').slick("slickPause");
-			// $('.suwarikuHomeMv-list').addClass('is-load');
-			// $('.suwarikuHomeMv-list').slick("slickPlay");
-		}
-	}
-	csrSlider();
-
-
-	function historySlider() {
-		if($('.suwarikuHistoryPresentSlider')[0]){
-			$('.suwarikuHistoryPresentSlider').slick({
-				fade: true,
-				slidesToShow: true,
-				slidesToScroll:1,
-				// centerMode: true,
-				adaptiveHeight: true,
-				initialSlide: 0, //start number
-				cssEase: 'ease-in-out',
-				infinite: true,
-				arrows: false,
-				dots: false,
-				autoplay: true,
-				autoplaySpeed: 2500,
-				speed: 1500,
-				slide: '.suwarikuHistoryPresentSliderItem',
-				// prevArrow: '<i class="m-icon-slide-prev">前へ</i>',
-				// nextArrow: '<i class="m-icon-slide-next">次へ</i>',
-				pauseOnHover: false,
-				draggable: false,
-				touchThreshold: 10,
-				swipe: false,
-				// swipeToSlide: false,
-				touchMove: false
-			});
-			// $('.suwarikuHomeMv-list').slick("slickPause");
-			// $('.suwarikuHomeMv-list').addClass('is-load');
-			// $('.suwarikuHomeMv-list').slick("slickPlay");
-		}
-	}
-	historySlider();
-
-
-
-	//scroll anima
+	//Add Scroll Animation
 	function historyAmina(){
 		if($('body').hasClass('suwarikuTHistory')){
 			var scroll = $(window).scrollTop();
@@ -275,8 +82,7 @@ common effect
 		historyAmina();
 	});
 
-
-	//scroll anima
+	//Add Scroll Home Animation
 	function homeAmina(){
 		if($('body').hasClass('suwarikuHome')){
 			var scroll = $(window).scrollTop();
@@ -318,9 +124,7 @@ common effect
 		homeAmina();
 	});
 
-
-
-	//scroll anima
+	//Add Scroll Service Animation
 	function serviceAnima(){
 		if($('.suwarikuJsServiceAnima')[0]){
 			var scroll = $(window).scrollTop();
@@ -347,24 +151,198 @@ common effect
 		serviceAnima();
 	});
 
+	//Pop Up
+	$('.js-puwin').on("click",function(e){
+		e.preventDefault();
+		window.open(this.href,"WindowName","width=520,height=520,resizable=yes,scrollbars=yes");
+		return false;
+	});
+
+	//Add Smooth Scroll
+	$.fn.smoothScroll = function(options){
+		var c = $.extend({
+			speed : 400,
+			easing : 'swing'
+		}, options);
+		this.on('click',function(e){
+			e.preventDefault();
+			var $elmHash = this.hash;
+			if(!$elmHash || $elmHash === "#") {
+				return false;
+			};
+			var targetOffset = $($elmHash).offset().top - $('.suwarikuHeader').innerHeight();
+			$('html,body').animate({scrollTop: targetOffset}, c.speed, c.easing);
+		});
+	}
+	$("a[href^='#']").smoothScroll();
+
+	function ankerSet(){
+		var urlHash = location.hash.replace(/<[^>]+>/g, '');
+		if(urlHash.indexOf('#') != -1 && $(urlHash)[0]){
+			$('html,body').animate({scrollTop: $(urlHash).offset().top - $('.suwarikuHeader').innerHeight()}, 1);
+		}
+	}
+	addLoadBrowser(ankerSet);
+
+	//Header Menu
+	$('.suwarikuHeaderMenu').on("click",function(e){
+		e.preventDefault();
+		$(this).toggleClass('is-active');
+		$('.suwarikuHeader').toggleClass('is-active');
+		$('.suwarikuHeadernav').toggleClass('is-active');
+		return false;
+	});
+
+	//Slider Warehouse
+	function warehouseDetailSlider() {
+		if($('.suwarikuServiceWarehouseDetailSlider')[0]){
+			$('.suwarikuServiceWarehouseDetailSlider').slick({
+				fade: true,
+				slidesToShow: true,
+				slidesToScroll:1,
+				// centerMode: true,
+				adaptiveHeight: true,
+				initialSlide: 0, //start number
+				cssEase: 'ease-in-out',
+				infinite: true,
+				arrows: false,
+				dots: true,
+				autoplay: true,
+				autoplaySpeed: 2000,
+				speed: 500,
+				slide: '.suwarikuServiceWarehouseDetailSliderItem',
+				// prevArrow: '<i class="m-icon-slide-prev">前へ</i>',
+				// nextArrow: '<i class="m-icon-slide-next">次へ</i>',
+				pauseOnHover: false,
+				draggable: false,
+				touchThreshold: 10,
+				swipe: false,
+				// swipeToSlide: false,
+				touchMove: false
+			});
+			// $('.suwarikuHomeMv-list').slick("slickPause");
+			// $('.suwarikuHomeMv-list').addClass('is-load');
+			// $('.suwarikuHomeMv-list').slick("slickPlay");
+		}
+	}
+	warehouseDetailSlider();
+
+	// Slider Realstate
+	function realestateDetailSlider() {
+		if($('.suwarikuServiceRealstateImg')[0]){
+			$('.suwarikuServiceRealstateImg').slick({
+				fade: true,
+				slidesToShow: true,
+				slidesToScroll:1,
+				// centerMode: true,
+				adaptiveHeight: true,
+				initialSlide: 0, //start number
+				cssEase: 'ease-in-out',
+				infinite: true,
+				arrows: false,
+				dots: true,
+				autoplay: true,
+				autoplaySpeed: 2000,
+				speed: 500,
+				slide: '.suwarikuServiceRealstateImgItem',
+				// prevArrow: '<i class="m-icon-slide-prev">前へ</i>',
+				// nextArrow: '<i class="m-icon-slide-next">次へ</i>',
+				pauseOnHover: false,
+				draggable: false,
+				touchThreshold: 10,
+				swipe: false,
+				// swipeToSlide: false,
+				touchMove: false
+			});
+			// $('.suwarikuHomeMv-list').slick("slickPause");
+			// $('.suwarikuHomeMv-list').addClass('is-load');
+			// $('.suwarikuHomeMv-list').slick("slickPlay");
+		}
+	}
+	realestateDetailSlider();
+
+
+	// Slider CSR
+	function csrSlider() {
+		if($('.suwarikuCsrSlider')[0]){
+			$('.suwarikuCsrSlider').slick({
+				fade: true,
+				slidesToShow: true,
+				slidesToScroll:1,
+				// centerMode: true,
+				adaptiveHeight: true,
+				initialSlide: 0, //start number
+				cssEase: 'ease-in-out',
+				infinite: true,
+				arrows: false,
+				dots: false,
+				autoplay: true,
+				autoplaySpeed: 2000,
+				speed: 1500,
+				slide: '.suwarikuCsrSliderItem',
+				// prevArrow: '<i class="m-icon-slide-prev">前へ</i>',
+				// nextArrow: '<i class="m-icon-slide-next">次へ</i>',
+				pauseOnHover: false,
+				draggable: false,
+				touchThreshold: 10,
+				swipe: false,
+				// swipeToSlide: false,
+				touchMove: false
+			});
+			// $('.suwarikuHomeMv-list').slick("slickPause");
+			// $('.suwarikuHomeMv-list').addClass('is-load');
+			// $('.suwarikuHomeMv-list').slick("slickPlay");
+		}
+	}
+	csrSlider();
+
+	// Slider History
+	function historySlider() {
+		if($('.suwarikuHistoryPresentSlider')[0]){
+			$('.suwarikuHistoryPresentSlider').slick({
+				fade: true,
+				slidesToShow: true,
+				slidesToScroll:1,
+				// centerMode: true,
+				adaptiveHeight: true,
+				initialSlide: 0, //start number
+				cssEase: 'ease-in-out',
+				infinite: true,
+				arrows: false,
+				dots: false,
+				autoplay: true,
+				autoplaySpeed: 2500,
+				speed: 1500,
+				slide: '.suwarikuHistoryPresentSliderItem',
+				// prevArrow: '<i class="m-icon-slide-prev">前へ</i>',
+				// nextArrow: '<i class="m-icon-slide-next">次へ</i>',
+				pauseOnHover: false,
+				draggable: false,
+				touchThreshold: 10,
+				swipe: false,
+				// swipeToSlide: false,
+				touchMove: false
+			});
+			// $('.suwarikuHomeMv-list').slick("slickPause");
+			// $('.suwarikuHomeMv-list').addClass('is-load');
+			// $('.suwarikuHomeMv-list').slick("slickPlay");
+		}
+	}
+	historySlider();
+
 	
-	//mail spam protection
+	//Add Mail Spam Protection
 	
 	function secretaddress(){
 		var ma = String.fromCharCode(51,107,97,110,114,105,64,102,117,107,117,121,97,109,97,45,115,111,117,107,111,46,99,111,46,106,112);
 		var mt = String.fromCharCode(109,97,105,108,116,111,58);
 		document.write('<a href="'+ mt + ma + '">' + ma + '</a>');
 	}
-	
-
-/*--------------------------------------------------------------------------
-heightLine * have to write last line
---------------------------------------------------------------------------*/
 
 	function heightLineSet() {
 		$('.suwarikuJsHLine').matchHeight();
 	}
-	addOnload(heightLineSet);
+	addLoadBrowser(heightLineSet);
 
 
 })(jQuery);
